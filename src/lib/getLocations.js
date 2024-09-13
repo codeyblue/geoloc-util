a2e78ae142ad4cf56d07ff247b4a18bd8f7b0073
Kyla Kolb <kkolb416@gmail.com>
2024-09-13 11:36:12 -0400

    catch city location errors

--- src/lib/getLocations.js
+++ src/lib/getLocations.js
@@ -11,8 +11,8 @@ async function getLocations(locations, API_KEY) {
   const cities = filterType(locations, 'city');
   const zipcodes = filterType(locations, 'zipcode');
 
-  const cityPromises = cities.map(async location => {
-    return await getLocationByCity(location, API_KEY);
+  const cityPromises = cities.map(async city => {
+    return await getLocationByCity(city, API_KEY);
   })
 
 
@@ -20,18 +20,21 @@ async function getLocations(locations, API_KEY) {
   return results;
 }
 
-async function getLocationByCity(location, API_KEY) {
-  const [city, state] = location.split(', ');
+async function getLocationByCity(cityState, API_KEY) {
+  const [city, state] = cityState.split(', ');
   return await fetch(`${baseUrl}/direct?q=${city},${state},US&limit=${limit}&appid=${API_KEY}`)
     .then(response => {
       return response.json();
     })
     .then(data => {
-      const {name, lat, lon, country, state} = data[0];
-      return {name, lat, lon, country, state};
+      if (data.length === 0) {
+        return {error: `No location found for ${city}, ${state}`, city, state};
+      }
+
+      return (({name, lat, lon, country, state}) => ({name, lat, lon, country, state}))(data[0]);
     })
     .catch(error => {
-      console.log(error)
+      return {error: error.message, city, state};
     });
 }
 
