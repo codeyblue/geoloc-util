bfbfb85bcf217ea1280df52d53f11a7a1ee8a23b
Kyla Kolb <kkolb416@gmail.com>
2024-09-13 13:39:44 -0400

    update base unit test

--- test/unit.test.js
+++ test/unit.test.js
@@ -4,7 +4,7 @@
     describe('Valid Inputs', () => {
       it('returns an object of the locations', async () => {
         const result = await cli(['Baltimore, MD', '33709', 'x']);
-        const expected = `[{name:'Baltimore',lat:39.2908816,lon:-76.610759,country:'US',state:'Maryland'},{name:'PinellasCounty',lat:27.8201,lon:-82.7308,country:'US'}]`;
+        const expected = `---Cities---Baltimore,MDName:BaltimoreLatitude:39.2908816Longitude:-76.610759Country:USState:Maryland---ZipCodes---33709Name:PinellasCountyLatitude:27.8201Longitude:-82.7308Country:US`;
         expect(result.stdout.replace(/[ \t\n\r]/gm, '')).toBe(expected);
       })
       it('it fetches data with a valid "city, state" format', () => {});
