60dbcf94dd2a0340fc4843d4d6eace577c6b7342
Kyla Kolb <kkolb416@gmail.com>
2024-09-13 13:37:11 -0400

    rename parselocation to parser

--- bin/index.js
+++ bin/index.js
@@ -4,13 +4,13 @@ import { program } from 'commander';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import parseLocation from '../src/utils/parseLocations.js';
+import Parser from '../src/utils/parser.js';
 import getLocations from '../src/lib/getLocations.js';
 
 program
   .version('1.0.0')
   .description('Fetch geolocation data for a list of locations')
-  .requiredOption('-l, --locations <locations...>', 'List of space-separated locations to look up', parseLocation, [])
+  .requiredOption('-l, --locations <locations...>', 'List of space-separated locations to look up', Parser.parseLocation, [])
   .option('-k, --key <apikey>', 'API key for OpenWeather, if not set in the .env or environment')
   .usage('--locations <locations...>');
 
